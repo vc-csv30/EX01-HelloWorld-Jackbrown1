55412643d597d54d57cafec65b200fc2c81221d0
lorenzo <lgutierrez7056@gmail.com>
2019-09-04 11:47:02 -0700

    Push

--- src/main.cc
+++ src/main.cc
@@ -8,10 +8,3 @@
  * @author Jackbrown1
  */
 
-#include <iostream>
-#include <csv30>
-
-int main(int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
-    return EXIT_SUCCESS;
-}
